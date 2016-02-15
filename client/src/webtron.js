7ea5f70dd57c9e15330b3fe5a5216687b87d652a
alecdwm <alec@owls.io>
2016-02-15 16:58:02 +0100

    Progress on class-ifying client

--- client/src/webtron.js
+++ client/src/webtron.js
@@ -2,25 +2,23 @@
 "use strict";
 
 // Variables
-var webtron, // global phaser instance
-	socket, // global socket connection
-	players = {}, // global array of connected players
+var	webtron,         // global reference to phaser instance
+	socket,          // global reference to socket connection
+	players = {},    // global array of connected players
 	playerData = {}, // data from main menu form
-	colorHexes = {
-		'blue': "#00c2cc",
-		'green': "#2ee5c7",
-		'orange': "#f2d91a",
-		'purple': "#8a2ee5",
-		'red': "#e5482e",
-		'white': "#e5feff",
-	},
-	colorHexesNums = {
+	colorToHex = {   // convert color name to hex value
 		'blue': 0x00c2cc,
 		'green': 0x2ee5c7,
 		'orange': 0xf2d91a,
 		'purple': 0x8a2ee5,
 		'red': 0xe5482e,
 		'white': 0xe5feff,
+	},
+	dirToRot = { // convert direction name to rotation value
+		'UP':    0,
+		'DOWN':  Math.PI,
+		'RIGHT': Math.PI / 2,
+		'LEFT':  3 * Math.PI / 2,
 	};
 
 // Initialize
@@ -116,10 +114,10 @@ function create() {
 	// Background
 	webtron.add.image(0, 0, "background")
 
-	// Player
-	// thisPlayer = new playerBike(250, 500, playerData.colour)
-
-	// Player Input
+	// // Player
+	// players[0] = new playerBike(250, 500, playerData.colour)
+	//
+	// // Player Input
 	// var keybinds = webtron.input.keyboard.addKeys({
 	// 	'up': Phaser.Keyboard.W,
 	// 	'left': Phaser.Keyboard.A,
@@ -133,23 +131,23 @@ function create() {
 	// 	'right': Phaser.Keyboard.RIGHT,
 	// })
 	//
-	// keybinds.up.onDown.add(thisPlayer.turnUp, thisPlayer)
-	// keybinds.left.onDown.add(thisPlayer.turnLeft, thisPlayer)
-	// keybinds.down.onDown.add(thisPlayer.turnDown, thisPlayer)
-	// keybinds.right.onDown.add(thisPlayer.turnRight, thisPlayer)
+	// keybinds.up.onDown.add(players[0].turnUp, players[0])
+	// keybinds.left.onDown.add(players[0].turnLeft, players[0])
+	// keybinds.down.onDown.add(players[0].turnDown, players[0])
+	// keybinds.right.onDown.add(players[0].turnRight, players[0])
 	//
-	// altKeybinds.up.onDown.add(thisPlayer.turnUp, thisPlayer)
-	// altKeybinds.left.onDown.add(thisPlayer.turnLeft, thisPlayer)
-	// altKeybinds.down.onDown.add(thisPlayer.turnDown, thisPlayer)
-	// altKeybinds.right.onDown.add(thisPlayer.turnRight, thisPlayer)
+	// altKeybinds.up.onDown.add(players[0].turnUp, players[0])
+	// altKeybinds.left.onDown.add(players[0].turnLeft, players[0])
+	// altKeybinds.down.onDown.add(players[0].turnDown, players[0])
+	// altKeybinds.right.onDown.add(players[0].turnRight, players[0])
 }
 
 function update() {
-	// thisPlayer.update(webtron.time.physicsElapsed);
+	// players[0].update(webtron.time.physicsElapsed);
 }
 
 // Entities
-function playerBike(initX, initY, color, speed, direction) {
+function playerBike(x, y, color, speed, direction) {
 	switch (color) {
 		case "blue":
 		case "green":
@@ -187,100 +185,97 @@ function playerBike(initX, initY, color, speed, direction) {
 			console.log("Error: " + direction + " is not a player direction!")
 			break;
 	}
-	this.directionEnum = {
-		UP: 0,
-		DOWN: Math.PI,
-		RIGHT: Math.PI / 2,
-		LEFT: 3 * Math.PI / 2,
-	}
+	//
+	// Phaser.Sprite.call(this, x, y, "lightbike-" + this.color)
+	// this.anchor = new Phaser.Point(0.5, 0.5)
+	// this.rotation = dirToRot[this.direction]
 
-	this.sprite = webtron.add.sprite(initX, initY, "lightbike-" + this.color)
+	this.sprite = webtron.add.sprite(x, y, "lightbike-" + this.color)
 	this.sprite.anchor = new Phaser.Point(0.5, 0.5);
-	this.sprite.rotation = this.directionEnum[this.direction]
+	this.sprite.rotation = dirToRot[this.direction]
 
 	this.trail = new lightTrail(this)
 
 	this.justTurned = true
 	this.alive = true
-
-	this.update = function(dt) {
-		if (!this.alive) {
-			return
-		}
-		if (this.direction === "UP") {
-			this.sprite.y -= this.speed * dt;
-		} else if (this.direction === "RIGHT") {
-			this.sprite.x += this.speed * dt;
-		} else if (this.direction === "DOWN") {
-			this.sprite.y += this.speed * dt;
-		} else if (this.direction === "LEFT") {
-			this.sprite.x -= this.speed * dt;
-		}
-		if (this.sprite.x < 0 || this.sprite.x > webtron.width ||
-			this.sprite.y < 0 || this.sprite.y > webtron.height) {
-			this.die();
-		}
-		// needs to check all trails, not just this bike's
-		if (!this.justTurned && this.trail.detectPtCollision(this.frontCollidePt())) {
-			this.die();
-		}
-		this.trail.update(this)
-		this.justTurned = false
+}
+playerBike.prototype.update = function(dt) {
+	if (!this.alive) {
+		return
 	}
-	this.turnUp = function() {
-		if (this.alive && this.direction != "DOWN" && this.direction != "UP") {
-			this.trail.setTurn(this)
-			this.direction = "UP";
-			this.sprite.rotation = this.directionEnum[this.direction]
-			this.justTurned = true
-		}
+	if (this.direction === "UP") {
+		this.sprite.y -= this.speed * dt;
+	} else if (this.direction === "RIGHT") {
+		this.sprite.x += this.speed * dt;
+	} else if (this.direction === "DOWN") {
+		this.sprite.y += this.speed * dt;
+	} else if (this.direction === "LEFT") {
+		this.sprite.x -= this.speed * dt;
 	}
-	this.turnLeft = function() {
-		if (this.alive && this.direction != "RIGHT" && this.direction != "LEFT") {
-			this.trail.setTurn(this)
-			this.direction = "LEFT";
-			this.sprite.rotation = this.directionEnum[this.direction]
-			this.justTurned = true
-		}
+	if (this.sprite.x < 0 || this.sprite.x > webtron.width ||
+		this.sprite.y < 0 || this.sprite.y > webtron.height) {
+		this.die();
 	}
-	this.turnDown = function() {
-		if (this.alive && this.direction != "UP" && this.direction != "DOWN") {
-			this.trail.setTurn(this)
-			this.direction = "DOWN";
-			this.sprite.rotation = this.directionEnum[this.direction]
-			this.justTurned = true
-		}
+	// needs to check all trails, not just this bike's
+	if (!this.justTurned && this.trail.detectPtCollision(this.frontCollidePt())) {
+		this.die();
 	}
-	this.turnRight = function() {
-		if (this.alive && this.direction != "LEFT" && this.direction != "RIGHT") {
-			this.trail.setTurn(this)
-			this.direction = "RIGHT";
-			this.sprite.rotation = this.directionEnum[this.direction]
-			this.justTurned = true
-		}
+	this.trail.update(this)
+	this.justTurned = false
+}
+playerBike.prototype.turnUp = function() {
+	if (this.alive && this.direction != "DOWN" && this.direction != "UP") {
+		this.trail.setTurn(this)
+		this.direction = "UP";
+		this.sprite.rotation = dirToRot[this.direction]
+		this.justTurned = true
 	}
-	this.frontCollidePt = function() {
-		switch (this.direction) {
-			case "UP":
-				return new Phaser.Point(this.sprite.x, this.sprite.y - 4)
-				break;
-			case "LEFT":
-				return new Phaser.Point(this.sprite.x - 4, this.sprite.y)
-				break;
-			case "RIGHT":
-				return new Phaser.Point(this.sprite.x + 4, this.sprite.y)
-				break;
-			case "DOWN":
-				return new Phaser.Point(this.sprite.x, this.sprite.y + 4)
-				break;
-		}
-		console.log("Player direction '" + this.direction + " is not valid")
-		return new Phaser.Point(this.sprite.x, this.sprite.y)
+}
+playerBike.prototype.turnLeft = function() {
+	if (this.alive && this.direction != "RIGHT" && this.direction != "LEFT") {
+		this.trail.setTurn(this)
+		this.direction = "LEFT";
+		this.sprite.rotation = dirToRot[this.direction]
+		this.justTurned = true
 	}
-	this.die = function() {
-		this.alive = false
-		this.speed = 0;
+}
+playerBike.prototype.turnDown = function() {
+	if (this.alive && this.direction != "UP" && this.direction != "DOWN") {
+		this.trail.setTurn(this)
+		this.direction = "DOWN";
+		this.sprite.rotation = dirToRot[this.direction]
+		this.justTurned = true
 	}
+}
+playerBike.prototype.turnRight = function() {
+	if (this.alive && this.direction != "LEFT" && this.direction != "RIGHT") {
+		this.trail.setTurn(this)
+		this.direction = "RIGHT";
+		this.sprite.rotation = dirToRot[this.direction]
+		this.justTurned = true
+	}
+}
+playerBike.prototype.frontCollidePt = function() {
+	switch (this.direction) {
+		case "UP":
+			return new Phaser.Point(this.sprite.x, this.sprite.y - 4)
+			break;
+		case "LEFT":
+			return new Phaser.Point(this.sprite.x - 4, this.sprite.y)
+			break;
+		case "RIGHT":
+			return new Phaser.Point(this.sprite.x + 4, this.sprite.y)
+			break;
+		case "DOWN":
+			return new Phaser.Point(this.sprite.x, this.sprite.y + 4)
+			break;
+	}
+	console.log("Player direction '" + this.direction + " is not valid")
+	return new Phaser.Point(this.sprite.x, this.sprite.y)
+}
+playerBike.prototype.die = function() {
+	this.alive = false
+	this.speed = 0;
 }
 
 function lightTrail(bike) {
@@ -291,11 +286,11 @@ function lightTrail(bike) {
 		case "purple":
 		case "red":
 		case "white":
-			this.color = colorHexes[bike.color]
+			this.color = colorToHex[bike.color]
 			break;
 
 		case null:
-			this.color = colorHexes.orange
+			this.color = colorToHex.orange
 			break;
 
 		default:
@@ -308,34 +303,34 @@ function lightTrail(bike) {
 		this.startX, this.startY, this.startX, this.startY
 	)]
 	this.graphics = webtron.add.graphics()
-	this.graphics.lineStyle(2, colorHexesNums[bike.color])
+	this.graphics.lineStyle(2, this.color)
 	this.graphics.moveTo(this.startX, this.startY)
+}
 
-	this.update = function(bike) {
-		this.lines[this.lines.length - 1].
-		setTo(this.startX, this.startY, bike.sprite.x, bike.sprite.y)
+lightTrail.prototype.update = function(bike) {
+	this.lines[this.lines.length - 1].
+	setTo(this.startX, this.startY, bike.sprite.x, bike.sprite.y)
 
-		this.graphics.lineStyle(2, colorHexesNums[bike.color])
-		this.graphics.lineTo(
-			this.lines[this.lines.length - 1].end.x,
-			this.lines[this.lines.length - 1].end.y)
-	}
+	this.graphics.lineStyle(2, this.color)
+	this.graphics.lineTo(
+		this.lines[this.lines.length - 1].end.x,
+		this.lines[this.lines.length - 1].end.y)
+}
 
-	this.setTurn = function(bike) {
-		this.update(bike)
-		this.startX = bike.sprite.x
-		this.startY = bike.sprite.y
-		this.lines.push(new Phaser.Line(
-			this.startX, this.startY, this.startX, this.startY
-		))
-	}
+lightTrail.prototype.setTurn = function(bike) {
+	this.update(bike)
+	this.startX = bike.sprite.x
+	this.startY = bike.sprite.y
+	this.lines.push(new Phaser.Line(
+		this.startX, this.startY, this.startX, this.startY
+	))
+}
 
-	this.detectPtCollision = function(point) {
-		for (var i = 0; i < this.lines.length; i++) {
-			if (this.lines[i].pointOnSegment(point.x, point.y)) {
-				return true
-			}
+lightTrail.prototype.detectPtCollision = function(point) {
+	for (var i = 0; i < this.lines.length; i++) {
+		if (this.lines[i].pointOnSegment(point.x, point.y)) {
+			return true
 		}
-		return false
 	}
+	return false
 }
