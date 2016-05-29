9404b6f9e050a7ed1b12c69c958f7d30e66a87bb
alecdwm <alec@owls.io>
2016-05-29 21:48:53 +0200

    compile sass with gulp, webkit doesn't select title

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,9 +1,20 @@
-var gulp = require('gulp')
-var ts = require('gulp-typescript')
+var gulp       = require('gulp')
+var cssmin     = require('gulp-cssmin')
+var sass       = require('gulp-sass')
 var sourcemaps = require('gulp-sourcemaps')
-var uglify = require('gulp-uglify')
+var ts         = require('gulp-typescript')
+var uglify     = require('gulp-uglify')
 
-gulp.task('default', function () {
+gulp.task('sass', function() {
+	return gulp.src('sass/webtron.s+(a|c)ss')
+		.pipe(sourcemaps.init())
+		.pipe(sass({errLogToConsole: true}))
+		.pipe(cssmin())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('bin'))
+})
+
+gulp.task('typescript', function() {
 	return gulp.src(['src/**/*.ts', '!src/**/*.d.ts'])
 		.pipe(sourcemaps.init())
 		.pipe(ts({
@@ -19,3 +30,10 @@ gulp.task('default', function () {
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('bin'))
 })
+
+gulp.task('watch', function() {
+	gulp.watch('sass/**/*.s+(a|c)ss', ['sass'])
+	gulp.watch('src/**/*.ts', ['typescript'])
+})
+
+gulp.task('default', ['sass', 'typescript'])
