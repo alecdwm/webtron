f35c949d9f805086e5bf6157f32b1fee2a051f7c
alecdwm <alec@owls.io>
2019-05-07 21:13:47 +1000

    ported client main menu to vanillajs

--- client/states/MainMenu.js
+++ client/states/MainMenu.js
@@ -1,66 +1,151 @@
 class MainMenu {
 	constructor() {
 		this.preload = [
-			'img/gridBG.png',
+			'img/gridbike-blue.png',
+			'img/gridbike-green.png',
 			'img/gridbike-orange.png',
+			'img/gridbike-purple.png',
+			'img/gridbike-red.png',
+			'img/gridbike-white.png',
 		]
 	}
 
 	onEnter() {
-		const {
-			loader,
-			scene,
-		} = this.game
+		const { loader, scene } = this.game
 
-		const gridBG = new PIXI.Sprite(loader.resources['img/gridBG.png'].texture)
-		scene.addChild(gridBG)
+		this.playerName = this.game.globalstate.playerName || ''
+		this.playerColor = this.game.globalstate.playerColor || Webtron.randomColor()
 
-		const devSpriteBike = new PIXI.Sprite(loader.resources['img/gridbike-orange.png'].texture)
-		devSpriteBike.anchor.set(0.5, 0.5)
-		devSpriteBike.position.set(280, 280)
-		devSpriteBike.rotation = 3*Math.PI/2
-		scene.addChild(devSpriteBike)
+		const uiTextProperties = { fontFamily: Webtron.uiFontFamily, fontSize: Webtron.uiFontSize, fill: 'white' }
+		const uiBoldTextProperties = Object.assign({}, uiTextProperties, { fontWeight: 'bold' })
 
-		this.devSpriteBike = devSpriteBike
-		this.playerName = new PIXI.Text('CLU', {
-			fontFamily: '"Courier New", Courier, monospace',
-			fontSize: 32,
-			fill: 'white',
-		})
-		this.playerName.anchor.set(0.5, 0.5)
-		this.playerName.position.set(280, 120)
-		scene.addChild(this.playerName)
+		this.nameLabel = new PIXI.Text('NAME:', uiTextProperties)
+		this.nameLabel.anchor.set(1.0, 0.5)
+		this.nameLabel.position.set(240, 210)
+		scene.addChild(this.nameLabel)
+
+		this.playerNamePreview = new PIXI.Text(`${this.playerName}_`, uiBoldTextProperties)
+		this.playerNamePreview.anchor.set(0.0, 0.5)
+		this.playerNamePreview.position.set(280, 210)
+		scene.addChild(this.playerNamePreview)
+
+		this.colorLabel = new PIXI.Text('COLOR:', uiTextProperties)
+		this.colorLabel.anchor.set(1.0, 0.5)
+		this.colorLabel.position.set(240, 270)
+		scene.addChild(this.colorLabel)
+
+		this.colorButtonLeft = new PIXI.Text('<', uiBoldTextProperties)
+		this.colorButtonLeft.anchor.set(0.0, 0.5)
+		this.colorButtonLeft.position.set(280, 270)
+		this.colorButtonLeft.interactive = true
+		this.colorButtonLeft.buttonMode = true
+		this.colorButtonLeft.on('click', this.setPreviousPlayerColor.bind(this))
+		scene.addChild(this.colorButtonLeft)
+
+		this.playerColorPreview = new PIXI.Sprite(loader.resources[`img/gridbike-${this.playerColor}.png`].texture)
+		this.playerColorPreview.anchor.set(0.5, 0.5)
+		this.playerColorPreview.position.set(340, 270)
+		this.playerColorPreview.rotation = (3 * Math.PI) / 2
+		scene.addChild(this.playerColorPreview)
+
+		this.colorButtonRight = new PIXI.Text('>', uiBoldTextProperties)
+		this.colorButtonRight.anchor.set(1.0, 0.5)
+		this.colorButtonRight.position.set(400, 270)
+		this.colorButtonRight.interactive = true
+		this.colorButtonRight.buttonMode = true
+		this.colorButtonRight.on('click', this.setNextPlayerColor.bind(this))
+		scene.addChild(this.colorButtonRight)
+
+		this.enterButton = new PIXI.Text('CONNECT', uiBoldTextProperties)
+		this.enterButton.anchor.set(0.5, 0.5)
+		this.enterButton.position.set(280, 350)
+		this.enterButton.interactive = true
+		this.enterButton.buttonMode = true
+		this.enterButton.on('click', this.connect.bind(this))
+		scene.addChild(this.enterButton)
+
+		this.statusText = new PIXI.Text(
+			this.game.globalstate.statusText || '',
+			Object.assign({}, uiBoldTextProperties, { fill: Webtron.colorToHexString['orange'] }),
+		)
+		this.statusText.anchor.set(0.5, 0.5)
+		this.statusText.position.set(280, 105)
+		scene.addChild(this.statusText)
 
 		this.ready = true
 	}
 
-	onUpdate(/*dt*/) {
-		// this.devSpriteBike.y -= 100*dt
-	}
+	//
+	// event callbacks
+	//
 
 	onKeyDown(key) {
 		switch (key) {
-		case 'Backspace':
-			this.playerName.text = this.playerName.text.substring(0, this.playerName.text.length-1)
-			break
-		case 'ArrowLeft':
-			console.log(key)
-			break
-		case 'ArrowRight':
-			console.log(key)
+			case 'Enter':
+			case 'Return':
+				this.connect()
+				break
+
+			case 'Backspace':
+				this.removeKeyFromPlayerName()
+				break
+
+			case 'ArrowLeft':
+				this.setPreviousPlayerColor()
+				break
+
+			case 'ArrowRight':
+				this.setNextPlayerColor()
+				break
 		}
 	}
 
 	onKeyPress(key) {
 		switch (key) {
-		case 'Enter':
-			this.game.changeState('Connect')
-			break
-		default:
-			this.playerName.text += key
+			case 'Enter':
+			case 'Return':
+				break
+
+			case ' ':
+				this.addKeyToPlayerName('_')
+				break
+
+			default:
+				this.addKeyToPlayerName(key.toLowerCase())
+				break
 		}
+	}
+
+	//
+	// helper methods
+	//
+
+	addKeyToPlayerName(key) {
+		this.playerName = (this.playerName + key).slice(0, Webtron.maxPlayerNameLength)
+		this.playerNamePreview.text = `${this.playerName}_`
+	}
+	removeKeyFromPlayerName() {
+		this.playerName = this.playerName.slice(0, -1)
+		this.playerNamePreview.text = `${this.playerName}_`
+	}
+
+	setNextPlayerColor() {
+		const { loader } = this.game
+		this.playerColor = Webtron.colors[(Webtron.colors.indexOf(this.playerColor) + 1) % Webtron.colors.length]
+		this.playerColorPreview.texture = loader.resources[`img/gridbike-${this.playerColor}.png`].texture
+	}
+	setPreviousPlayerColor() {
+		const { loader } = this.game
+		const currentIndex = Webtron.colors.indexOf(this.playerColor)
+		this.playerColor = Webtron.colors[(currentIndex > 0 ? currentIndex : Webtron.colors.length) - 1]
+		this.playerColorPreview.texture = loader.resources[`img/gridbike-${this.playerColor}.png`].texture
+	}
+
+	connect() {
+		this.game.setGlobalState({ playerName: this.playerName || 'ANON', playerColor: this.playerColor })
+		this.game.changeState('Connect')
 	}
 }
 
-window.states = {}
+window.states = window.states || {}
 window.states.MainMenu = MainMenu
--- client/webtron.js
+++ client/webtron.js
@@ -1,58 +1,37 @@
-window.addEventListener('load', () => {
-	PIXI.utils.skipHello()
-	console.log(`Pixi.js ${PIXI.VERSION} - http://www.pixijs.com/`)
-
-	const protocol = window.location.protocol === 'https' ? 'wss' : 'ws'
-	const socket_url = `${protocol}://${window.location.host}/ws`
-	const socket = new WebSocket(socket_url)
-	socket.addEventListener('open', () => {
-		console.log('socket open')
-		socket.send(JSON.stringify("ListGames"))
-		// socket.send(new ArrayBuffer(12))
-	})
-	socket.addEventListener('error', error => {
-		console.log('socket error', error)
-	})
-	socket.addEventListener('message', event => {
-		const message = event.data
-		console.log(`socket message: ${message}`)
-	})
-	socket.addEventListener('close', event => {
-		console.log(`socket closed: ${event.code} ${event.reason}`)
-	})
-
-	new Webtron(document.getElementById('webtron'))
-})
+window.addEventListener('load', () => new Webtron(document.getElementById('webtron')))
 
 class Webtron {
-	constructor(domElement) {
-		if (!domElement) {
-			domElement = document.body
-		}
+	constructor(domElement = document.body) {
+		// connect to server
+		this.createWebsocketConnection()
 
-		// set up asset loader
+		// Skip PIXI.js hello message,
+		// but still print an unobtrusive version when not in development.
+		PIXI.utils.skipHello()
+		if (window.location.hostname !== 'localhost') console.log(`PixiJS ${PIXI.VERSION} - http://www.pixijs.com/`)
+
+		// setup asset loader
 		this.loader = new PIXI.loaders.Loader()
 
-		// set up input events
+		// setup input events
 		document.addEventListener('keydown', this.onKeyDown.bind(this))
 		document.addEventListener('keyup', this.onKeyUp.bind(this))
 		document.addEventListener('keypress', this.onKeyPress.bind(this))
-		// document.addEventListener('mousedown', this.onMouseDown.bind(this))
-		// document.addEventListener('mouseup', this.onMouseUp.bind(this))
-		// document.addEventListener('mousemove', this.onMouseMove.bind(this))
-		// document.addEventListener('touchstart', this.onTouchStart.bind(this))
-		// document.addEventListener('touchend', this.onTouchEnd.bind(this))
-		// document.addEventListener('touchmove', this.onTouchMove.bind(this))
 
-		// set up renderer
-		this.renderer = new PIXI.autoDetectRenderer({
-			width: 560,
-			height: 560,
-			transparent: true,
-		})
+		// setup renderer
+		this.renderer = new PIXI.autoDetectRenderer({ width: 560, height: 560, transparent: true })
 		domElement.appendChild(this.renderer.view)
+		this.renderer.view.addEventListener('contextmenu', e => e.preventDefault())
 
-		// set up scene
+		// setup global state
+		this.globalstate = {
+			playerName: '',
+			playerColor: undefined,
+			socket: undefined,
+			statusText: '',
+		}
+
+		// setup scene
 		this.scene = new PIXI.Container()
 
 		// start main menu
@@ -63,39 +42,9 @@ class Webtron {
 		this.update()
 	}
 
-	changeState(newState) {
-		if (typeof(newState) === 'string') {
-			if (window.states[newState]) {
-				newState = window.states[newState]
-			} else {
-				throw new Error(`No state by name '${newState}' exists!`)
-			}
-		}
-
-		if (this.state) {
-			this.state.onExit && this.state.onExit()
-			this.loader.reset()
-		}
-
-		this.state = new newState()
-		this.state.game = this
-
-		if (this.state.preload && Array.isArray(this.state.preload)) {
-			this.loader
-				.add(this.state.preload)
-				.load((loader, resources) => {
-					for (let resourceKey in resources) {
-						const resource = resources[resourceKey]
-						if (resource.error) {
-							console.error(`${resource.error} (assetUrl: ${resource.url})`)
-						}
-					}
-					this.state.onEnter && this.state.onEnter.call(this.state)
-				})
-		} else {
-			this.state.onEnter && this.state.onEnter.call(this.state)
-		}
-	}
+	//
+	// update loop
+	//
 
 	update() {
 		this.updateNow = Date.now()
@@ -110,21 +59,114 @@ class Webtron {
 		window.requestAnimationFrame(this.update.bind(this))
 	}
 
+	//
+	// event callbacks
+	//
+
 	onKeyDown(e) {
-		if (this.state && this.state.onKeyDown) {
-			this.state.onKeyDown.call(this.state, e.key)
-		}
+		if (!this.state || !this.state.onKeyDown) return
+		this.state.onKeyDown.call(this.state, e.key)
 	}
 
 	onKeyUp(e) {
-		if (this.state && this.state.onKeyUp) {
-			this.state.onKeyUp.call(this.state, e.key)
-		}
+		if (!this.state || !this.state.onKeyUp) return
+		this.state.onKeyUp.call(this.state, e.key)
 	}
 
 	onKeyPress(e) {
-		if (this.state && this.state.onKeyPress) {
-			this.state.onKeyPress.call(this.state, e.key)
-		}
+		if (!this.state || !this.state.onKeyPress) return
+		this.state.onKeyPress.call(this.state, e.key)
 	}
+
+	//
+	// helper methods
+	//
+
+	setGlobalState(newState) {
+		this.globalstate = Object.assign(this.globalstate, newState)
+	}
+
+	createWebsocketConnection() {
+		const protocol = window.location.protocol === 'https' ? 'wss' : 'ws'
+		const socket_url = `${protocol}://${window.location.host}/ws`
+		const socket = new WebSocket(socket_url)
+		socket.addEventListener('open', () => {
+			console.log('socket open')
+			socket.send(JSON.stringify('ListGames'))
+		})
+		socket.addEventListener('error', error => {
+			console.error('socket error', error)
+		})
+		socket.addEventListener('message', event => {
+			if (typeof event.data !== 'string') {
+				console.warn('Received non-text websocket message', event)
+				return
+			}
+
+			const message = JSON.parse(event.data)
+			console.log(`socket message`, message)
+		})
+		socket.addEventListener('close', event => {
+			console.log(`socket closed: ${event.code} ${event.reason}`)
+		})
+	}
+
+	changeState(nextState) {
+		// get reference to next state
+		if (typeof nextState !== 'string') throw new Error(`nextState must be a string, given ${typeof nextState}`)
+		if (!window.states[nextState]) throw new Error(`No state by name '${nextState}' exists!`)
+		nextState = window.states[nextState]
+
+		// handle current state exit (if exists)
+		if (this.state) {
+			this.state.onExit && this.state.onExit()
+			this.loader.reset()
+		}
+
+		// reset scene
+		// this.scene = new PIXI.Container()
+
+		// switch states
+		this.state = new nextState()
+		this.state.game = this
+
+		// handle new state entry (if no asset preloading required)
+		if (!this.state.preload || !Array.isArray(this.state.preload)) {
+			this.state.onEnter && this.state.onEnter.call(this.state)
+			return
+		}
+
+		// handle asset preloading
+		this.loader.add(this.state.preload).load((loader, resources) => {
+			// print asset preloading errors
+			Object.values(resources).forEach(resource => {
+				if (resource.error) console.error(`${resource.error} (assetUrl: ${resource.url})`)
+			})
+
+			// handle new state entry
+			this.state.onEnter && this.state.onEnter.call(this.state)
+		})
+	}
+}
+
+Webtron.colors = ['blue', 'green', 'orange', 'purple', 'red', 'white']
+Webtron.randomColor = () => Webtron.colors[parseInt(Math.random() * Webtron.colors.length)]
+Webtron.maxPlayerNameLength = 12
+Webtron.uiFontFamily = '"Courier New", Courier, monospace'
+Webtron.uiFontSize = 32
+Webtron.colorToHex = {
+	blue: 0x00c2cc,
+	green: 0x2ee53d,
+	orange: 0xf2d91a,
+	purple: 0x8a2ee5,
+	red: 0xe5482e,
+	white: 0xe5feff,
+}
+Webtron.colorToHexString = {
+	blue: '#00c2cc',
+	green: '#2ee53d',
+	orange: '#f2d91a',
+	purple: '#8a2ee5',
+	red: '#e5482e',
+	white: '#e5feff',
 }
