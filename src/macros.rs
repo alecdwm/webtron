2f7b1e301d5a8446d5a76225c613bfc62ec21fa2
alecdwm <alec@owls.io>
2019-09-10 23:39:13 +1000

    removed handle_pattern! and handle_none!

--- src/macros.rs
+++ src/macros.rs
@@ -1,30 +1,5 @@
 ///
 /// A macro to handle those cases where you want to gracefully
-/// deal with a pattern like `Result::Err` or `Option::None`,
-/// but cannot use `try!` because your function does not return
-/// either of the `Result` or `Option` types.
-///
-/// The expression provided as the first parameter with be matched
-/// against the pattern provided as the second parameter.
-/// If the pattern matches, the expression provided as the third
-/// parameter will be evaluated and its result returned from the
-/// calling function.
-///
-/// Helps to prevent unnecessary code indentation.
-///
-#[macro_export]
-macro_rules! handle_pattern {
-    ( $expression:expr, $pattern:pat, $on_match:expr ) => {{
-        let result = $expression;
-        if let $pattern = result {
-            return $on_match;
-        }
-        result.unwrap()
-    }};
-}
-
-///
-/// A macro to handle those cases where you want to gracefully
 /// deal with a `Result::Err`, but cannot use `try!` because your
 /// function does not return the `Result` or `Option` type.
 ///
@@ -59,29 +34,5 @@ macro_rules! unwrap_or_return {
             return $on_error(error);
         }
         result.unwrap()
-    }};
-}
-
-///
-/// A macro to handle those cases where you want to gracefully
-/// deal with an `Option::None`, but cannot use `try!` because
-/// your function does not return the `Option` type.
-///
-/// The expression provided as the first parameter with be matched
-/// against the pattern `None`.
-/// If the pattern matches, the closure provided as the second
-/// parameter will be called with the expression result.
-/// The closure result will then be returned from the calling function.
-///
-/// Helps to prevent unnecessary code indentation.
-///
-#[macro_export]
-macro_rules! handle_none {
-    ( $expression:expr, $on_none:expr ) => {{
-        let option = $expression;
-        if let None = option {
-            return $on_none();
-        }
-        option.unwrap()
     }};
 }
