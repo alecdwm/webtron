e158fb62cac6b5c912d39cd45a58abd192c1e7a1
alecdwm <alec@owls.io>
2020-04-06 22:09:54 +1000

    fixed capitalisation of default player name

--- client/src/utils/defaultPlayer.js
+++ client/src/utils/defaultPlayer.js
@@ -1,7 +1,7 @@
 import { randomColor } from 'utils/colors'
 
 export default function defaultPlayer(player) {
-  if (!player.name) player.name = 'CLU'
+  if (!player.name) player.name = 'clu'
   if (!player.color) player.color = randomColor()
   return player
 }
