a9fce2d65f3527edd14538246461019e4e399699
alecdwm <alec@owls.io>
2020-04-09 13:24:46 +1000

    reduced ARENA_START_TIMER_SECONDS to 1 second

--- src/server/arena.rs
+++ src/server/arena.rs
@@ -20,7 +20,7 @@ use crate::server::{ArenaId, ArenaPoint, Direction, Line, Player, PlayerId};
 const ARENA_WIDTH: usize = 560;
 const ARENA_HEIGHT: usize = 560;
 const ARENA_MAX_PLAYERS: usize = 8;
-const ARENA_START_TIMER_SECONDS: i64 = 5;
+const ARENA_START_TIMER_SECONDS: i64 = 1;
 const LIGHTCYCLE_SPEED: isize = 1;
 
 #[derive(Debug, Clone, Serialize)]
