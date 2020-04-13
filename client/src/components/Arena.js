28e75b91add22635e63eedb45614dd07d45c41e5
alecdwm <alec@owls.io>
2020-04-13 22:18:25 +1000

    added touchscreen controls

--- client/src/components/Arena.js
+++ client/src/components/Arena.js
@@ -8,7 +8,7 @@ import usePreloadImages from 'hooks/usePreloadImages'
 import useStore from 'hooks/useStore'
 import useStoreDispatch from 'hooks/useStoreDispatch'
 import backgroundPanel from 'img/background-panel.svg'
-import React, { useCallback } from 'react'
+import React, { useCallback, useRef } from 'react'
 import lightcycleImages from 'utils/lightcycleImages'
 
 import styles from './Arena.module.css'
@@ -20,39 +20,17 @@ export default function Arena() {
   const dispatch = useStoreDispatch()
 
   const onStart = useCallback(() => dispatch(start()), [dispatch])
-  const onTurn = useCallback((direction) => dispatch(turn(direction)), [dispatch])
 
-  const onKeyDown = useCallback(
-    (event) => {
-      switch (event.key) {
-        case 'ArrowUp':
-        case 'w':
-          event.preventDefault()
-          return onTurn('up')
-        case 'ArrowLeft':
-        case 'a':
-          event.preventDefault()
-          return onTurn('left')
-        case 'ArrowRight':
-        case 'd':
-          event.preventDefault()
-          return onTurn('right')
-        case 'ArrowDown':
-        case 's':
-          event.preventDefault()
-          return onTurn('down')
-      }
-    },
-    [onTurn],
-  )
-  useEventListener('keydown', onKeyDown)
+  const arenaRef = useRef()
+  useKeyControls(arena.started)
+  useTouchControls(arena.started, arenaRef)
 
   const Arena = useClassName(styles.arena)
   const Background = useClassName([styles.background, arena.started !== null && styles.backgroundStarted])
   const StartButton = useClassName(styles.startButton, MenuButton)
 
   return (
-    <Arena>
+    <Arena ref={arenaRef}>
       <Background style={{ backgroundImage: `url(${backgroundPanel})` }} />
 
       {Object.entries(arena.lightribbons).map(([id, { points }]) => (
@@ -73,4 +51,107 @@ export default function Arena() {
       {arena.started === null ? <StartButton onClick={onStart}>START</StartButton> : null}
     </Arena>
   )
+}
+
+function useTurnCallback() {
+  const dispatch = useStoreDispatch()
+  return useCallback((direction) => dispatch(turn(direction)), [dispatch])
+}
+
+function useKeyControls(started) {
+  const onTurn = useTurnCallback()
+
+  const onKeyDown = useCallback(
+    (event) => {
+      if (!started) return
+
+      switch (event.key) {
+        case 'ArrowUp':
+        case 'w':
+          event.preventDefault()
+          return onTurn('up')
+        case 'ArrowLeft':
+        case 'a':
+          event.preventDefault()
+          return onTurn('left')
+        case 'ArrowRight':
+        case 'd':
+          event.preventDefault()
+          return onTurn('right')
+        case 'ArrowDown':
+        case 's':
+          event.preventDefault()
+          return onTurn('down')
+      }
+    },
+    [started],
+  )
+  useEventListener('keydown', onKeyDown)
+}
+
+function useTouchControls(started, arenaRef) {
+  const onTurn = useTurnCallback()
+
+  const turnFromCenterOffset = useCallback(
+    (xFromCenter, yFromCenter) => {
+      const xDistance = Math.abs(xFromCenter)
+      const yDistance = Math.abs(yFromCenter)
+
+      if (xDistance < yDistance) {
+        if (yFromCenter < 0) {
+          return onTurn('up')
+        } else {
+          return onTurn('down')
+        }
+      } else {
+        if (xFromCenter < 0) {
+          return onTurn('left')
+        } else {
+          return onTurn('right')
+        }
+      }
+    },
+    [onTurn],
+  )
+
+  const onTouchStart = useCallback(
+    (event) => {
+      if (!started) return
+
+      const touch = event.changedTouches[0]
+      if (!touch) return
+
+      event.preventDefault()
+
+      const rect = event.currentTarget.getBoundingClientRect()
+      const x = touch.clientX - rect.left
+      const y = touch.clientY - rect.top
+      const xFromCenter = x - rect.width / 2
+      const yFromCenter = y - rect.height / 2
+
+      turnFromCenterOffset(xFromCenter, yFromCenter)
+    },
+    [started, turnFromCenterOffset],
+  )
+
+  const onMouseDown = useCallback(
+    (event) => {
+      if (!started) return
+
+      const rect = event.currentTarget.getBoundingClientRect()
+
+      const x = event.clientX - rect.left
+      const y = event.clientY - rect.top
+      const xFromCenter = x - rect.width / 2
+      const yFromCenter = y - rect.height / 2
+
+      turnFromCenterOffset(xFromCenter, yFromCenter)
+    },
+    [started, turnFromCenterOffset],
+  )
+
+  useEventListener('touchstart', onTouchStart, arenaRef.current)
+  useEventListener('mousedown', onMouseDown, arenaRef.current)
+
+  return arenaRef
 }
--- client/src/hooks/useEventListener.js
+++ client/src/hooks/useEventListener.js
@@ -4,6 +4,6 @@ export default function useEventListener(event, callback, element = window) {
   useEffect(() => {
     element.addEventListener(event, callback)
 
-    return () => window.removeEventListener(event, callback)
+    return () => element.removeEventListener(event, callback)
   }, [event, callback, element])
 }
