679f9f6d20a6e56fad507171195c56ea8f7c93b0
alecdwm <alec@owls.io>
2020-04-15 04:28:58 +1000

    switched from suspense to nextjs dynamic import to prevent ssr

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -6,7 +6,7 @@ import { StoreContext } from 'hooks/useStore'
 import { StoreDispatchContext } from 'hooks/useStoreDispatch'
 import useThunkReducer from 'hooks/useThunkReducer'
 import PropTypes from 'prop-types'
-import React, { Suspense } from 'react'
+import React from 'react'
 
 const isSSR = typeof window === 'undefined'
 
@@ -21,14 +21,10 @@ if (!isSSR && module.hot)
 export default function App({ Component, pageProps }) {
   const [store, dispatch] = useThunkReducer(reducers, initialStore)
 
-  if (isSSR) return null
-
   return (
     <StoreContext.Provider value={store}>
       <StoreDispatchContext.Provider value={dispatch}>
-        <Suspense fallback={<dev />}>
-          <Component {...pageProps} />
-        </Suspense>
+        <Component {...pageProps} />
       </StoreDispatchContext.Provider>
     </StoreContext.Provider>
   )
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,9 @@
-import Webtron from 'components/Webtron'
 import useAddWebtronLoadedClass from 'hooks/useAddWebtronLoadedClass'
 import usePreventWebtronContextMenu from 'hooks/usePreventWebtronContextMenu'
+import dynamic from 'next/dynamic'
 import React from 'react'
+
+const Webtron = dynamic(() => import('components/Webtron'), { ssr: false })
 
 export default function IndexPage() {
   usePreventWebtronContextMenu()
